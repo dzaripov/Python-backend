cd38e3b56dcee8b59d20845479edf0dea2ce6abb
dzaripov <daniszaripov02@mail.ru>
2024-09-21 12:57:34 +0300

    task done

--- HW1/README.md
+++ HW1/README.md
@@ -1,5 +1,9 @@
 # HW 1
 
+Установка зависимостей (там только uvicorn)
+
+`pip install -r requirements.txt`
+
 Запуск приложения
 
-uvicorn main:app --reload --port 8000
+`uvicorn main:app --reload --port 8000`
--- HW1/main.py
+++ HW1/main.py
@@ -1,12 +1,17 @@
 import pprint
+import math
+
+import json
+from urllib.parse import parse_qs
+
 
 def fib(n):
-    if n == 1:
-        return 0
-    if n == 2:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+    arr = [0] * (n + 2)
+    arr[0] = 0
+    arr[1] = 1
+    for i in range(2, n + 1):
+        arr[i] = arr[i-1] + arr[i-2]
+    return arr[n]
 
 
 async def send_response(send, status, body, content_type='text/plain'):
@@ -19,29 +24,72 @@ async def send_response(send, status, body, content_type='text/plain'):
     })
     await send({
         'type': 'http.response.body',
-        'body': body.encode('utf-8'),
+        'body': str(body).encode('utf-8')
     })
 
- 
+
+async def get_request_body(receive):
+    body = b''
+    more_body = True
+    while more_body:
+        message = await receive()
+        body += message.get('body', b'')
+        more_body = message.get('more_body', False)
+
+    return body
+
+
 async def app(scope, receive, send):
     if scope['method'] != 'GET':
         response_body = 'Not Found'
         await send_response(send, 404, response_body)
-    
-    print(pprint.pprint(scope))
+        return
+        
     path = scope['path'].strip('/').split('/')
+    query_string = scope["query_string"].decode('utf-8')
+    
     if len(path) == 2 and path[0] == 'fibonacci':
         # будем считать, что последовательность начинается с 0
-        try:
+        if path[1].lstrip('-').isdigit():
             n = int(path[1])
-            response_body = fib(n)
-            await send_response(send, 200, response_body)
-        except ValueError:
-            await send_response(send, 400, 'Invalid number format')
-    elif len(path) == 2 and path[0] == 'factorial':
-        pass
-    elif len(path) == 2 and path[0] == 'mean':
-        pass
+            if n < 0:
+                response_body = 'Negative number, only non-negative are supported'
+                await send_response(send, 400, response_body)
+            else:
+                response_body = {'result': fib(n)}
+                await send_response(send, 200, response_body)
+        else:
+            await send_response(send, 422, 'Invalid number format')
+
+    elif path[0] == 'factorial':
+        try:
+            n = int(parse_qs(query_string).get('n')[0])
+            if n < 0:
+                response_body = 'Negative number, only non-negative are supported'
+                await send_response(send, 400, response_body)
+            else:
+                response_body = {'result': math.factorial(n)}
+                await send_response(send, 200, response_body)
+        except:
+            response_body = 'Unprocessed entity'
+            await send_response(send, 422, response_body)
+
+    elif path[0] == 'mean':
+        body = await get_request_body(receive)
+        try:
+            ns = json.loads(body.decode('utf-8'))
+            
+            if not ns:
+                response_body = 'Bad Request'
+                await send_response(send, 400, response_body)
+            else:
+                response_body = {'result': sum(ns) / len(ns)}
+                await send_response(send, 200, response_body)
+
+        except json.JSONDecodeError:
+            response_body = 'Unprocessable Entity'
+            await send_response(send, 422, response_body)
+
     else:
         response_body = 'Not Found'
         await send_response(send, 404, response_body)
